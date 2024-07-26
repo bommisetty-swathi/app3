329c411e9b6055c7f51aa7db315f34030f02e068
bommisetty swathi <bommisettyswathi@gmail.com>
2024-07-26 06:36:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        items: [],
+        DataisLoaded: false,
+    };
 }
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res)=>res.json())
+    .then((json)=>{
+      this.setState({
+        items:json,
+        DataisLoaded:true,
+      });
+    });
+  }
+  render(){
+    const{DataisLoaded,items}=this.state;
+    if(!DataisLoaded)
+      return(
+    <div>
+      <h1>need some time to load the page</h1>
+    </div>
+    );
+    return(
+      <div className="app3">
+        <h1 className="geek">Geeks for Geeks</h1>
+        <h3>welcome to geeks for geeks</h3>
+        <div className="container">
+          {items.map((items)=>(
+            <div className="items">
+              <ol key={items.id}>
+                <div>
+                  <strong>
+                    {"userName:"},
+                  </strong>
+                  {items.username},
+                </div>
+                <div>
+                  Full_name:{items.name},
+                </div>
+                <div>
+                  user_email:{items.email}
+                </div>
+              </ol>
+            </div>
 
+          
+
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
